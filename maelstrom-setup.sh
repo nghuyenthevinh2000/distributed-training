01347cbd5d75deab72e01ca366c5eb25a3583ad4
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2023-03-29 11:35:17 +0700

    update maelstrom script setup

--- maelstrom-setup.sh
+++ maelstrom-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget https://github.com/jepsen-io/maelstrom/releases/download/v0.2.2/maelstrom.tar.bz2
+wget https://github.com/jepsen-io/maelstrom/releases/download/v0.2.3/maelstrom.tar.bz2
 tar -xf maelstrom.tar.bz2
